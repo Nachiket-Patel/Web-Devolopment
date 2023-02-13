ef902f43cd9d1b9b102fc0766abe5c8d7ece1f33
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-02-13 18:39:41 +0530

    variables

---
+++ JavaScript/W3schools/10-variables.js
@@ -0,0 +1,43 @@
+//methods variables can define
+var a = 10;
+let b = 20;
+const c = 30;
+d = 40;
+console.log(a);
+console.log(b);
+console.log(c);
+console.log(d);
+
+
+let price1 = 5;
+let price2 = 10;
+const total = price1 + price2;
+console.log(total);
+
+
+//JavaScript is case-censetive
+var Hello = 1;
+var hello = 2;
+console.log(Hello);
+console.log(hello);
+
+
+var carName;
+console.log(carName);
+carName = "BMW";
+console.log(carName);
+
+
+//If you put a number in quotes, the rest of the numbers will be treated as strings, and concatenated.
+let x = "5" + 2 + 5;
+console.log(x);
+let y = 2 + 5 + "5";
+console.log(y);// where number 2+5=7 and then string"5" comes and entire number becomes string
+//If you put a number in quotes, the rest of the numbers will be treated as strings, and concatenated.
+
+
+// underscore and dollor are treated as variables
+var _ = 10;
+let $ = "dollor";
+console.log(_);
+console.log($);
