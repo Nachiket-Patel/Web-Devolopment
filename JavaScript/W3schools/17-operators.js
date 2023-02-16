58919ce356e19101a3a1f5dac38c8feeeb264e01
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-02-16 17:14:44 +0530

    operators

--- JavaScript/W3schools/17-operators.js
+++ JavaScript/W3schools/17-operators.js
@@ -1,6 +1,11 @@
 let x = 5 + 5;
 console.log(x);
+//when adding number to string JavaScript will treat number as string.
 let y = "5" + 5;
 console.log(y);
 let z = "Hello" + 5;
 console.log(z);
+
+//to check datatype of variable or value typeof operator is used.
+console.log(typeof(x));
+console.log(typeof(""));
