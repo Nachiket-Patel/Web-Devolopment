6995971d2f83517612daf9c40403603fc86295dc
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-04-03 16:01:53 +0530

    Add UpperCase button and use states

--- React/my-app/src/components/TextForm.js
+++ React/my-app/src/components/TextForm.js
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { PropTypes } from "prop-types";
 
 export default function TextForm(props){
+
+    const [text,setText] = useState("Enter text here");
+
+    function handleUpClick(){
+        console.log("upper case button was clicked"); //to check in console that function is working or not
+        const upperText = text.toUpperCase();
+        setText(upperText);
+    }
+
+    function handleOnChange(event){
+        console.log("On change event"); //to check in console that function is working or not
+        setText(event.target.value) ;
+    }
+
     return(
         <div> 
             <h1>{props.heading}</h1> 
             <div className="mb-3 my-3" >
-                <textarea className="form-control" id="textbox" rows="10"></textarea>
+                <textarea className="form-control" value={text} onChange={handleOnChange} id="textbox" rows="10"></textarea>
             </div>
-            <button className="btn btn-primary">Convert to Uppercase</button>
+            <button className="btn btn-primary" onClick={handleUpClick}>Convert to Uppercase</button>
         </div>
     );
 };
