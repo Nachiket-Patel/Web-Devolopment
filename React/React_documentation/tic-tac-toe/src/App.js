e4111a3b2db2bb02cf693264cd4886e4e0140a6b
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-04-07 16:02:47 +0530

    Building board create nine squares

--- React/React_documentation/tic-tac-toe/src/App.js
+++ React/React_documentation/tic-tac-toe/src/App.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './App.css';
 
 
-export default function square(){
+export default function Board(){
   return(
-    <button className='square'>X</button>
+    <div className="board">
+      <div className="board-row">
+        <button className='square'>1</button>
+        <button className='square'>2</button>
+        <button className='square'>3</button>
+      </div>
+      <div className="board-row">
+        <button className='square'>4</button>
+        <button className='square'>5</button>
+        <button className='square'>6</button>
+      </div>
+      <div class="board-row">
+        <button className='square'>7</button>
+        <button className='square'>8</button>
+        <button className='square'>9</button>
+      </div>  
+    </div>
   )
 }
