740a84fa4ec22f74884815191263fcd7de84f36c
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-04-18 12:43:43 +0530

    Refactor code as per documentation

--- React/React_documentation/tic-tac-toe/src/App.js
+++ React/React_documentation/tic-tac-toe/src/App.js
@@ -9,28 +9,6 @@ function Square({ value, onSquareClick }){
 }
 
 
-function calculateWinner(squares){  
-  const lines = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
-  ];
-
-  for(let i=0; i<lines.length; i++){
-    const [a, b, c] = lines[i];
-    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
-      return squares[a];
-    }
-  }
-  return null;
-}
-
-
 function Board({ xIsNext, squares, onPlay }){
 
   function handleClick(i){
@@ -86,7 +64,7 @@ function Board({ xIsNext, squares, onPlay }){
 export default function Game(){
 
   const [xIsNext, setXIsNext] = useState(true);
-  const [history, setHistory] = usestate([Array(9).fill(null)]);
+  const [history, setHistory] = useState([Array(9).fill(null)]);
   const currentSquares = history[history.length - 1];
 
   function handlePlay(nextSquares){
@@ -104,4 +82,26 @@ export default function Game(){
       </div>
     </div>
   );
+}
+
+
+function calculateWinner(squares){  
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+
+  for(let i=0; i<lines.length; i++){
+    const [a, b, c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
 }
