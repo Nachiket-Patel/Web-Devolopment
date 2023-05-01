bbe279c66514935d5f97634a2650daec2e811a24
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-05-01 18:02:19 +0530

    Remove React default files

---
+++ React/W3schools/w3schools/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Nachiket Patel</h1>
+        <p>This is tutorials from W3schools</p>
+      </div>
+    );
+  }
+}
+
+export default App;
