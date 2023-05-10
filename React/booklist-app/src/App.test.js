679db067bb3dbdc32cbfface8235d9589965d604
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-05-10 12:10:21 +0530

    Delete old project

--- React/booklist-app/src/App.test.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import BookList from './BookList.js';
-
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<BookList />, div);
-});
