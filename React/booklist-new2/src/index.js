725af4fe1aebaac20da5ff2659222a63b95893ba
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-05-15 12:11:50 +0530

    Use map method to render list of books

--- React/booklist-new2/src/index.js
+++ React/booklist-new2/src/index.js
@@ -48,8 +48,8 @@ const books = [
     
     {
         title : 'Ikigai',
-    img : thirdImg,
-    author : 'Francesc Miralles and Hector Garcia',
+        img : thirdImg,
+        author : 'Francesc Miralles and Hector Garcia',
     },
 
     {
@@ -71,28 +71,6 @@ const books = [
     },
 ];
 
-//* adding map method to access list
-
-//* first method
-const name = ['Raju', 'Babu', 'Shyaam'];
-//create new variable for new list
-const newName = name.map((name) => {
-    //look into console for names
-    console.log(name);
-});
-
-//* second method
-const BookList = () => {
-    return (
-        <section className='booklist'>
-            {/* not need to create new variable, use map mathod in same variable itself. */}
-            {name.map((name) => {
-                return <h1>{name}</h1>;
-            })}
-        </section>
-    );
-};
-
 
 const Books = ({ title, image, author }) => {
     return(
@@ -105,21 +83,23 @@ const Books = ({ title, image, author }) => {
 };
 
 
-//* comment this block so error don't show up
-// export default function BookList(){
+//* let's use map method in BookList to get books from list
+export default function BookList(){
 
-//     return(
-//         <section className='booklist'>    
-//             <Books title={firstBook.title} image={firstBook.img} author={firstBook.author} />
-//             <Books title={secondBook.title} image={secondBook.img} author={secondBook.author} />
-//             <Books title={thirdBook.title} image={thirdBook.img} author={thirdBook.author} />
-//             <Books title={fourthBook.title} image={fourthBook.img} author={fourthBook.author} />
-//             <Books title={fifthBook.title} image={fifthBook.img} author={fifthBook.author} />
-//             <Books title={sixthBook.title} image={sixthBook.img} author={sixthBook.author} />
-//         </section>
-//     );
+    return(
+        //* We don't need to use Books six times, map mathod will use Books as many books are in list
+        <section className='booklist'>    
+            {books.map((book) => {
+                const { img, title, author } = book;
+                return (
+                    <Books title={title} image={img} author={author} />
+                    
+                );
+            })}
+        </section>
+    );
 
-// };
+};
 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
