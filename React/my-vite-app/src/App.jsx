020348e4a2adefeccb0ddd1f74f02194745cd2db
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-05-25 12:29:18 +0530

    First React app with Vite

---
+++ React/my-vite-app/src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+import First from './First_component';
+
+
+function App() {
+
+  return (
+    <>
+      <h1>This my first vite app</h1>
+      <First />
+    </>
+  )
+}
+
+export default App
---
+++ React/my-vite-app/src/First_component.jsx
@@ -0,0 +1,8 @@
+export default function First_Component() {
+  return(
+    <div>
+      <h2>I am Nachiket</h2>
+      <p>Hello, World!</p>
+    </div>
+  );
+}
