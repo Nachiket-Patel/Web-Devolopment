68ba24bc0adfcacfc586c8692defcce7ce846169
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-05-25 14:41:52 +0530

    vite docs

---
+++ React/vite.js
@@ -0,0 +1,12 @@
+//* Create new vite project
+// -> npm create vite@latest
+
+//* create new vite react app command - This will create react app
+// -> npm create vite@latest name-of-app -- --template react
+
+//* instead of .js extention here will be .jsx extention
+
+//* index.html is in source not in public.
+
+//* instead of index.js here is main.jsx. In create-react-app you can name files .js or  .jsx but in vite it must be .jsx
+
