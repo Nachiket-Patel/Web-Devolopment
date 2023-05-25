e9f4e899c3f7f8a549d912c36b5ba95c19b27af0
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-05-25 15:40:21 +0530

    hook rules

---
+++ React/hooks_rules.js
@@ -0,0 +1,8 @@
+/*
+* start with "use" (both react and custom hooks).
+* Components must be in Uppercase.
+* invoke inside function/components body.
+* Don't call hooks inside conditions like if-else.
+* set function don't update state immidiately 
+
+*/
