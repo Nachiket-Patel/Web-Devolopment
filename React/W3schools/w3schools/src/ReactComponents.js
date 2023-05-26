4b1b1608bc719ce85ed8393ad1d5eb40c272d2dd
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-05-26 15:15:11 +0530

    Apply CSS

--- React/W3schools/w3schools/src/ReactComponents.js
+++ React/W3schools/w3schools/src/ReactComponents.js
@@ -4,21 +4,21 @@ import React from 'react';
 class Car extends React.Component {
   render() {
     return(
-    <h2>This Car is class based component.</h2>
+    <h2 className='class'>This Car is class based component.</h2>
     );
   };
 };
 
 function Fruits() {
   return(
-    <h2>This Fruits is function based component</h2>
+    <h2 className='function'>This Fruits is function based component</h2>
   );
 }
 
 function InComponent() {
   return(
     <>
-      <h2>This is In Component</h2>
+      <h2 className='component'>This is In Component</h2>
       <h3>We can use component inside another component</h3>
       <p>This is component in component.</p>
     </>
@@ -28,7 +28,7 @@ function InComponent() {
 function OutComponent() {
   return(
     <React.Fragment>
-      <h2>This is outside component</h2>
+      <h2 className='component'>This is outside component</h2>
       <p>we can call in component here.</p>
       <InComponent />
     </React.Fragment>
