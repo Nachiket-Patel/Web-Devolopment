5bad46dfe17e2afc4fd3225728885b533defdd06
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-05-26 12:04:43 +0530

    change class based app to function based, delete file

--- React/W3schools/w3schools/src/App.js
+++ React/W3schools/w3schools/src/App.js
@@ -1,15 +1,13 @@
-import React, { Component } from 'react';
+import React from 'react';
 import './App.css';
 
-class App extends Component {
-  render() {
-    return (
-      <div>
-        <h1>Nachiket Patel</h1>
-        <p>This is tutorials from W3schools</p>
-      </div>
-    );
-  }
+export default function App() {
+    
+  return (
+    <div>
+      <h1>Nachiket Patel</h1>
+      <p>This is tutorials from W3schools</p>
+    </div>
+  );
+  
 }
-
-export default App;
