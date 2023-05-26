474d68ffd4a54a13c30ae42659cfc552b4cd52f0
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-05-26 12:33:23 +0530

    Add Class based component

--- React/W3schools/w3schools/src/App.js
+++ React/W3schools/w3schools/src/App.js
@@ -1,12 +1,23 @@
 import React from 'react';
 import './App.css';
 
+
+class Car extends React.Component {
+  render() {
+    return(
+      <h2>This Car class based components</h2>
+    );
+  };
+};
+
+
 export default function App() {
     
   return (
     <div>
       <h1>Nachiket Patel</h1>
       <p>This is tutorials from W3schools</p>
+      <Car />
     </div>
   );
   
