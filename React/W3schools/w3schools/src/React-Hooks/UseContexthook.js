dcf9d5fd80ecc6ba61ecae0595af12d60bb67f27
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-06-08 12:33:42 +0530

    solution with useContext hook

--- React/W3schools/w3schools/src/React-Hooks/UseContexthook.js
+++ React/W3schools/w3schools/src/React-Hooks/UseContexthook.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, createContext, useContext } from 'react';
 
 
 const ProblemUseState = () => {
@@ -8,7 +8,7 @@ const ProblemUseState = () => {
 
     return(
       <>
-        <h1>{`Hello ${user}!`}</h1>
+        <h2>{`Hello ${user}!`}</h2>
         <Component2 user={user} />
       </>
     );
@@ -17,7 +17,7 @@ const ProblemUseState = () => {
   function Component2({ user }) {
     return(
       <>
-        <h1>Component 2</h1>
+        <h2>Component 2</h2>
         <Component3 user={user} />
       </>
     );
@@ -26,7 +26,7 @@ const ProblemUseState = () => {
   function Component3({ user }) {
     return(
       <>
-        <h1>Component 3</h1>
+        <h2>Component 3</h2>
         <Component4 user={user} />
       </>
     );
@@ -35,7 +35,7 @@ const ProblemUseState = () => {
   function Component4({ user }) {
     return(
       <>
-        <h1>Component 4</h1>
+        <h2>Component 4</h2>
         <Component5 user={user} />
       </>
     );
@@ -44,8 +44,69 @@ const ProblemUseState = () => {
   function Component5 ({ user }) {
     return(
       <>
-        <h1>Component 5</h1>
-        <h2>{`Hello ${user} again!`}</h2>
+        <h2>Component 5</h2>
+        <h3>{`Hello ${user} again!`}</h3>
+      </>
+    );
+  }
+
+  return(
+    <>
+      <Component1 />
+    </>
+  );
+}
+
+
+function SolutionUseState() {
+
+  const UserContext = createContext();
+
+  function Component1() {
+    const [user, setUser] = useState("Walter White");
+
+    return(
+      <UserContext.Provider value={user}>
+        <h2>{`Hello ${user}!`}</h2>
+        <Component2 />
+      </UserContext.Provider>
+    );
+  }
+
+  function Component2() {
+    return(
+      <>
+        <h2>Component 2</h2>
+        <Component3 />
+      </>
+    );
+  }
+
+  function Component3() {
+    return(
+      <>
+        <h2>Component 3</h2>
+        <Component4 />
+      </>
+    );
+  }
+
+  function Component4() {
+    return(
+      <>
+        <h2>Component 4</h2>
+        <Component5 />
+      </>
+    );
+  }
+
+  function Component5() {
+    const user = useContext(UserContext);
+
+    return(
+      <>
+        <h2>Component 5</h2>
+        <h3>{`Hello ${user} again!`}</h3>
       </>
     );
   }
@@ -61,11 +122,13 @@ const ProblemUseState = () => {
 export default function UseContexthook() {
   return(
     <>
-      <h2>Problem with using only useState hook</h2>
+      <h1>Problem with using only useState hook</h1>
       <p>we have many nested components. The component at the top and bottom of the stack need access to the state.</p>
       <p>To do this without Context, we will need to pass the state as "props" through each nested component. This is called "prop drilling".</p>
       <ProblemUseState />
       <br/>
+      <h1>Solution with using useContext hook</h1>
+      <SolutionUseState />
     </>
   );
 }
