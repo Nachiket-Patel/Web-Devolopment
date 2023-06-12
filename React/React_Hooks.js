ef8ba4a21757c3a886db6befbb98065d317a32b1
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-06-12 16:41:31 +0530

    React Hooks info

--- React/React_Hooks.js
+++ React/React_Hooks.js
@@ -6,3 +6,22 @@
 * set function don't update state immidiately 
 
 */
+
+/*
+        *Hook                   Use
+    1-useState    -    To track values and update value in UI. 
+
+    2-useEffect     -   To apply side effect to useState hook, to control its effects.
+                    -   fetching data, directly updating the DOM, and timers.
+                    -   No dependency passed: Runs on every render.
+                    -   empty array: Runs only on the first render.
+                    -   Props or state values: Runs on the first render, And any time any dependency value  changes.
+
+
+    3-useContext    -   To pass prop to only specific component rather than passing on every component.
+
+    4-useRef    -   It works as useState but updating it won't trigger re-render.
+                -   Accessing DOM Elements.
+                -   Tracking State Changes to keep track of previous state values.
+
+    */
