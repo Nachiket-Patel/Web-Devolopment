9c5d51b905ac19cf35cc99673f8465f94cdc1a4d
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-06-13 12:01:05 +0530

    useReducer Hook

--- React/W3schools/w3schools/src/App.js
+++ React/W3schools/w3schools/src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import './App.css';
-import UseRefHook from './React-Hooks/UseRefHook';
-
+import UseReducerHook from './React-Hooks/UseReducerHook';
 
 
 export default function App() {
@@ -10,7 +9,7 @@ export default function App() {
     <div>
       <h1>Nachiket Patel</h1>
       <p>This is tutorials from W3schools</p><br/>
-      <UseRefHook />
+      <UseReducerHook />
     </div>
   );
   
