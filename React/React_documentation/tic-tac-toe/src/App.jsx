9ff7a23bce1bd7d76c0eabecd8e5a52e8e0285a3
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-06-30 16:14:35 +0530

    create first component

--- React/React_documentation/tic-tac-toe/src/App.jsx
+++ React/React_documentation/tic-tac-toe/src/App.jsx
@@ -1,6 +1,13 @@
 import './App.css';
 
 
+function Square() {
+  return(
+    <button className='square'>X</button>
+  );
+}
+
+
 function App() {
 
   return (
@@ -8,19 +15,19 @@ function App() {
       <h1 style={{textAlign: 'center'}}>Tic Tac Toe</h1>
       <section>
         <div className="board-row">
-          <button className="square">1</button>
-          <button className="square">2</button>
-          <button className="square">3</button>
+          <Square />
+          <Square />
+          <Square />
         </div>
         <div className="board-row">
-          <button className="square">4</button>
-          <button className="square">5</button>
-          <button className="square">6</button>
+          <Square />
+          <Square />
+          <Square />
         </div>
         <div className="board-row">
-          <button className="square">7</button>
-          <button className="square">8</button>
-          <button className="square">9</button>
+          <Square />
+          <Square />
+          <Square />
         </div>
       </section>
     </>
