13fb110c0af93e2737cf0dbf763c6ae3962f9ad5
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-06-30 16:09:00 +0530

    create table of 9 squares

--- React/React_documentation/tic-tac-toe/src/App.jsx
+++ React/React_documentation/tic-tac-toe/src/App.jsx
@@ -5,7 +5,24 @@ function App() {
 
   return (
     <>
-      <h1>Tic Tac Toe</h1>
+      <h1 style={{textAlign: 'center'}}>Tic Tac Toe</h1>
+      <section>
+        <div className="board-row">
+          <button className="square">1</button>
+          <button className="square">2</button>
+          <button className="square">3</button>
+        </div>
+        <div className="board-row">
+          <button className="square">4</button>
+          <button className="square">5</button>
+          <button className="square">6</button>
+        </div>
+        <div className="board-row">
+          <button className="square">7</button>
+          <button className="square">8</button>
+          <button className="square">9</button>
+        </div>
+      </section>
     </>
   );
 }
