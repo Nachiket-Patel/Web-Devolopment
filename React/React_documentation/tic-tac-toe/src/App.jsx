9fef3c7a8fcd1c18d70cca7f05094f07094662cd
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-07-07 16:02:18 +0530

    calulate winner

--- React/React_documentation/tic-tac-toe/src/App.jsx
+++ React/React_documentation/tic-tac-toe/src/App.jsx
@@ -9,14 +9,36 @@ function Square({ value, onSquareClick }) {
 }
 
 
+function win(squares) {
+
+  const lines = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,4,8],
+    [2,4,6],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+  ];
+
+  for(let i=0; i<lines.length; i++){
+    const [a,b,c] = lines[i];
+    if(squares[a] && squares[a] === squares[b] && squares[a] === squares[c]){
+      return squares[a];
+    }
+  }
+  return null;
+}
+
+
 function App() {
   const [squares, setSquares] = useState(Array(9).fill(null));
   const [x, setX] = useState(true);
 
-
   function handleClick(i) {
   
-    if(squares[i]){
+    if(squares[i] || win(squares)){
       return;
     }
 
@@ -29,10 +51,16 @@ function App() {
   }
 
 
+  const winner = win(squares);
+  let status;
+  winner ? status = "Winner: " + winner : status = "Next player: " + (x ? "X" : "0");
+
+
   return (
     <>
       <h1 style={{textAlign: 'center'}}>Tic Tac Toe</h1>
       <section>
+      <div className="status">{status}</div>
         <div className="board-row">
           <Square  value={squares[0]} onSquareClick={() => handleClick(0)} />
           <Square  value={squares[1]} onSquareClick={() => handleClick(1)} />
