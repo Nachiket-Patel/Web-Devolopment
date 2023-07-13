2d41bef881fb136c3165d9cddb47b5710db130bf
Nachiket-Patel <new.computer.sci.tech@gmail.com>
2023-07-13 16:32:41 +0530

    Project 4

--- React/projects/src/App.jsx
+++ React/projects/src/App.jsx
@@ -1,12 +1,12 @@
-import Project3 from './project_3/Project3';
+import Project4 from './project_4/project4';
 
 
 function App() {
 
   return (
     <>
-      <h1 style={{textAlign: 'center'}}>Project 3</h1>
-      <Project3 />
+      <h1 style={{textAlign: 'center'}}>Project 4</h1>
+      <Project4 />
     </>
   )
 }
